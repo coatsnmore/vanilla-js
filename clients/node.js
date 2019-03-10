7609ae93532513a113c86f0556ddb13ce1ab1cb5
coatsnmore <coats.18@gmail.com>
2019-03-09 19:17:35 -0500

    enhanced node client to do multiple calls in async method; also added simpler promise.all reusing async functions;

--- clients/node.js
+++ clients/node.js
@@ -2,6 +2,16 @@ const fetch = require('node-fetch');
 const port = 4000;
 const host = `http://localhost:${port}`;
 
+// let json = {
+//     'name':'Nick',
+//     'age':'13',
+//     'location':'USA'
+// }
+
+// let {name, age, location} = json;
+
+// console.log(`name: ${name}\nage: ${age}\nlocation: ${location}`);
+
 // fetch(`${host}/books`)
 //     .then(promise => promise.json())
 //     .then(json => console.log(`json: ${JSON.stringify(json)}`));
@@ -18,27 +28,50 @@ async function getSomeBooks() {
 
 // getSomeBooks().then(books => console.log(`books: ${JSON.stringify(books)}`));
 
-// fetch('http://localhost:4000/slow-books')
-//     .then(promise => promise.json())
-//     .then(json => console.log(`slow books: ${json}`));
+async function getSlowBooks() {
+    const p = await fetch(`${host}/slow-books`);
+    const json = await p.json();
+    const { books } = json;
 
-const books = fetch(`${host}/books`)
-const slowBooks = fetch(`${host}/slow-books`)
-const authors = fetch(`${host}/authors`)
+    //resolves promise with whatever is returned
+    return books;
+}
 
-Promise.all([books, slowBooks, authors]).then(values  => {
-    values[0].json().then(books => {
-        console.log('\x1b[36m%s\x1b[0m', `books:`);
-        console.log(books);
-    });
+// return promise
+async function getAllBooks() {
+    const booksResponse = await fetch(`${host}/books`);
+    const slowBooksResponse = await fetch(`${host}/slow-books`);
+    const booksJSON = await booksResponse.json();
+    const slowBooksJSON = await slowBooksResponse.json();
+    const books = [booksJSON.books, slowBooksJSON.books];
 
-    values[1].json().then(slowBooks => {
-        console.log('\x1b[36m%s\x1b[0m', `slow books:`);
-        console.log(slowBooks);
-    });
+    //resolves promise with whatever is returned
+    return books;
+}
 
-    values[2].json().then(authors => {
-        console.log('\x1b[36m%s\x1b[0m', `authors:`);
-        console.log(authors);
-    });
+// getAllBooks().then(books => console.log(`books: ${JSON.stringify(books)}`));
+
+Promise.all([getSomeBooks(), getSlowBooks()]).then(values  => {
+    console.log(`BOOKS:  ${JSON.stringify(values[0].concat(values[1]))}`);
 });
+
+// const books = fetch(`${host}/books`)
+// const slowBooks = fetch(`${host}/slow-books`)
+// const authors = fetch(`${host}/authors`)
+
+// Promise.all([books, slowBooks, authors]).then(values  => {
+//     values[0].json().then(books => {
+//         console.log('\x1b[36m%s\x1b[0m', `books:`);
+//         console.log(books);
+//     });
+
+//     values[1].json().then(slowBooks => {
+//         console.log('\x1b[36m%s\x1b[0m', `slow books:`);
+//         console.log(slowBooks);
+//     });
+
+//     values[2].json().then(authors => {
+//         console.log('\x1b[36m%s\x1b[0m', `authors:`);
+//         console.log(authors);
+//     });
+// });
